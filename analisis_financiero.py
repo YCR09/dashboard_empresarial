7f0672dbbd44608f2fe696f5b5a23b4d9ba49a5a
YCR09 <yamhar2010@gmail.com>
2026-07-09 22:37:24 +0200

    Refactor code to improve readability and structure

--- analisis_financiero.py
+++ analisis_financiero.py
@@ -251,4 +251,4 @@ def finanzas():
                 )
       
     else:
-        st.warning("👈 Ve a la sección del menú 📤 Información y sube el archivo Excel a analizar")
+        st.warning("👈 Ve a la sección del menú 📤 Información en **Fuente de Datos** y selecciona una opción de datos para analizar")
